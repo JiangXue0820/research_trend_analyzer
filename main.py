4ee704c83536a204bdaef69ecb11eae612ae637c
ShirleyJ <shirley930820@gmail.com>
2025-08-03 21:49:09 +0200

    update paper_analyze tools

--- main.py
+++ main.py
@@ -2,12 +2,40 @@
 import logging
 from langfuse import get_client
 from langfuse.langchain import CallbackHandler
+from langchain.memory import ConversationBufferMemory
+from langchain_experimental.plan_and_execute import PlanAndExecute, load_agent_executor, load_chat_planner
 
 from configs import config
+from configs.llm_provider import get_llm
 from configs.logging import configure_logging
-from agents.conversation_agent import master_agent
+from tools.paper_fetch_tools_sql import paper_fetch_toolkit
+from tools.paper_analyze_tools import paper_analyze_toolkit
+
 
 configure_logging()  # Make sure logging is set up first
+
+# 1. Initialize LLM
+config.LLM_PROVIDER = 'gemini'
+llm = get_llm(config)
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+
+# 1. Create the planner (LLM decides on multi-step plan)
+planner = load_chat_planner(llm)
+
+# 2. Create the executor (agent capable of tool execution)
+executor = load_agent_executor(
+    llm=llm,
+    tools=paper_fetch_toolkit+paper_analyze_toolkit,
+    verbose=True
+)
+
+# 3. Combine into plan-and-execute agent
+master_agent = PlanAndExecute(
+    planner=planner,
+    executor=executor,
+    memory=memory,
+    verbose=True
+)
 
 if __name__ == '__main__':
     langfuse = get_client(public_key=config.LANGFUSE_API_KEY_PUBLIC)
